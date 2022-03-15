8ee16b4599ea1d637270a8b98e8352a4a220bf5d
Vasiliy Sharapov <vasiliys@gmail.com>
2022-03-15 08:41:57 -0400

    WIP

--- data.aws_ami.elk.tf
+++ data.aws_ami.elk.tf
@@ -3,8 +3,17 @@ data "aws_ami" "elk" {
 
   filter {
     name   = "name"
-    values = [var.ami_name]
+    values = ["Open Distro for Elasticsearch-1.13.2-x86_64-04/07/21-18.42.33"]
   }
-
-  owners = [data.aws_caller_identity.current.account_id]
 }
+
+#data "aws_ami" "elk" {
+#  most_recent = true
+#
+#  filter {
+#    name   = "name"
+#    values = [var.ami_name]
+#  }
+#
+#  owners = [data.aws_caller_identity.current.account_id]
+#}
